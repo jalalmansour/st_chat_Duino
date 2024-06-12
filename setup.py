3441c6fbfd2c8aabe583d86de5587d9887e7dfd6
Jalal Mansour <42254243+jalalmansour@users.noreply.github.com>
2024-06-12 16:09:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,18 @@ setuptools.setup(
     name="streamlit-Duino",
     version="0.1.0",
     author="Jalal Mansour",
-    author_email="jalalmansour663@gmail.com",
+    author_email="jalalmansour664@gmail.com",
     description="Streamline your workflow with our intuitive chat bar that fosters seamless communication and effortless file importing, empowering you to collaborate like never before and take your user experience to the next level.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    classifiers=[],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independant",
+    "License :: OSI Approved :: MIT License"
+    ],
     python_requires=">=3.7",
     install_requires=["streamlit>=1.2", "jinja2"],
 )
